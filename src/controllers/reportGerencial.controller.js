6c83732dd36360df8b14167628958386e0d1a7fa
Johann Gomez <johanngomez02@gmail.com>
2021-06-26 12:51:50 -0500

    consulta fecha

--- src/controllers/reportGerencial.controller.js
+++ src/controllers/reportGerencial.controller.js
@@ -7,9 +7,19 @@ const reportGerencial = async (req, res) => {
     let dateInit = dayjs(req.body.date).startOf('month').format('YYYY-MM-DD HH:mm:ss')
     let dateFinish = dayjs(req.body.date).endOf('month').format('YYYY-MM-DD HH:mm:ss')
 
-    let homeworksss = await HomeworkModel.find().where('createdAt').gte(dateInit).lte(dateFinish)
+    let monthHomeworks = await HomeworkModel.find().where('createdAt').gte(dateInit).lte(dateFinish)
     
-    res.json(homeworksss)
+    console.log(estadoHomework)
+    
+    res.json(monthHomeworks)
+    /*
+    for (let i = 0; i <= monthHomeworks; i++){
+        count = i++    
+        console.log(count)
+    }
+
+    
+*/
 }
 
 
